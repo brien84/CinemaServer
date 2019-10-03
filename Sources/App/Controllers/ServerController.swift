716ce9e0ec2fc621bc6adbabd79967ebd7bc5478
brien84 <houseinblues@gmail.com>
2019-10-04 00:38:04 +0300

    Add City enum in Constants

--- Sources/App/Controllers/ServerController.swift
+++ Sources/App/Controllers/ServerController.swift
@@ -18,6 +18,6 @@ final class ServerController {
     }
     
     func start() {
-        
+
     }
 }
---
+++ Sources/App/Utilities/Constants.swift
@@ -0,0 +1,13 @@
+//
+//  Constants.swift
+//  App
+//
+//  Created by Marius on 02/10/2019.
+//
+
+enum City: String {
+    case vilnius = "Vilnius"
+    case kaunas = "Kaunas"
+    case klaipeda = "Klaipėda"
+    case siauliai = "Šiauliai"
+}
