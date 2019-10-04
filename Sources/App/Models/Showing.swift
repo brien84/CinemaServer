1f86e15fbfc505db9dca340cd7f1c05088e6932d
brien84 <houseinblues@gmail.com>
2019-10-04 16:28:19 +0300

    Add Showing class

---
+++ Sources/App/Models/Showing.swift
@@ -0,0 +1,14 @@
+//
+//  Showing.swift
+//  App
+//
+//  Created by Marius on 04/10/2019.
+//
+
+import Foundation
+
+struct Showing: Codable {
+    let city: String
+    let date: Date
+    let venue: String
+}
