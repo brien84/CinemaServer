9e2fd581a2adea182fafd89eb214500010e72aab
brien84 <houseinblues@gmail.com>
2019-10-08 19:07:05 +0300

    Add explicit TimeZone in convertToDate() String extension

--- Sources/App/Utilities/Extensions.swift
+++ Sources/App/Utilities/Extensions.swift
@@ -29,6 +29,9 @@ extension String {
     func convertToDate() -> Date? {
         let dateFormatter = DateFormatter()
         
+        guard let timeZone = TimeZone(identifier: "Europe/Vilnius") else { fatalError("TimeZone not found!") }
+        dateFormatter.timeZone = timeZone
+        
         // ForumCinemas format: 19.09.2019 11:00
         dateFormatter.dateFormat = "dd'.'MM'.'yyyy' 'HH':'mm"
         if let date = dateFormatter.date(from: self) {
