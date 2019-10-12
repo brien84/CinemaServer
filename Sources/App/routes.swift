5fa53cb7fa8954fbab61a26f5171c238fdded677
brien84 <houseinblues@gmail.com>
2019-10-12 23:37:39 +0300

    Add routing

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -1,6 +1,8 @@
 import Vapor
 
-/// Register your application's routes here.
 public func routes(_ router: Router) throws {
- 
+    
+    router.get("movies") { req -> Future<[Movie]> in
+        return Movie.query(on: req).all()
+    }
 }
