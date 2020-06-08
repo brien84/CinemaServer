55158832b52363f63f3681cf7bd3a091134f6858
brien84 <houseinblues@gmail.com>
2020-06-08 22:05:35 +0300

    Create `posters` route for serving images

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -5,4 +5,12 @@ public func routes(_ router: Router) throws {
     router.get("all") { req -> Future<[Movie]> in
         return Movie.query(on: req).all()
     }
+
+    router.get("posters", String.parameter) { req -> Future<Response> in
+        let fileName = try req.parameters.next(String.self)
+        let path = try req.make(DirectoryConfig.self).workDir + "Public/Posters/" + fileName
+
+        return try req.streamFile(at: path)
+    }
+
 }
